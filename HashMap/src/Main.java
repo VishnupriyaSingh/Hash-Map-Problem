27884f8bc1289a97a9d474c736f3812c0ac68465
Vishnupriya <priya.singh9349@gmail.com>
2023-11-29 22:39:45 +0530

    Added code for frequency of words in a large paragraph

--- HashMap/src/Main.java
+++ HashMap/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         MyHashTable<String> hashTable = new MyHashTable<>();
-        String sentence = "To be or not to be";
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         String[] words = sentence.toLowerCase().split("\\s+");
 
         for (String word : words) {
